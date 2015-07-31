9160d57dc9b068f6adf5c33290e87654441be1ef
harringg <elists@ghmultimedia.com>
2015-07-31 09:43:55 -0500

    Re-wrote the script from ground up

--- CAServer-SET-ADUserX509Cert.ps1
+++ CAServer-SET-ADUserX509Cert.ps1
@@ -1,29 +1,26 @@
-function Set-ADUserX509Cert {
+function Set-ADUSER_X509_CERT {
 	<#
 	.SYNOPSIS
-		Takes the first.last.cer certificate and adds it to the AD first.last-adm altsecurityidentities Attribute
+		Adds the PIV Cert information to the ADUC EP user using the HSPDPID number
 	
 	.DESCRIPTION
-		This is the Powershell equivilent of ADUC > first.last-adm > Right-Click > Name Mapping... > Add
-        Collects a folder full of Lincpass Certificates (*.cer) and adds them to the Elevated Privelage AD account
+		Default Description
 	
 	.PARAMETER param1
 		param1 Description
 	
 	.EXAMPLE
-				PS C:\> Set-ADUserX509Cert -PRODUCTION REVIEW
+				PS C:\> Get-Function -param1 'Value1'
 	
 	.NOTES
-		    NAME: Set-ADUserX509Cert
+		    NAME: Set-ADUSER_X509_CERT
             AUTHOR: Grant Harrington
             EMAIL: grant.harrington@ars.usda.gov
-            CREATED: 7/2/2015 5:34 PM
-			LASTEDIT: 7/2/2015 8:01 PM
-            KEYWORDS: ADUC, MultiFactorAuthentication
+            CREATED: 7/7/2015 6:39 AM
+			LASTEDIT: 7/7/2015 10:02 PM
+            KEYWORDS: ADUC, PIV, MFA, CERT, HSPDPID
 	.LINK
-		Some code modified from:
-        https://social.technet.microsoft.com/Forums/lync/en-US/e021e05f-a3b4-43ea-aeb9-487d648477eb/set-ad-user-security-identity-mapping-of-x509-certificate-from-cer-file?forum=winserverpowershell
-
+		EAD Scripts
 #>
 	[CmdletBinding(SupportsPaging = $true,
 				   SupportsShouldProcess = $true)]
@@ -36,52 +33,55 @@ function Set-ADUserX509Cert {
 	)
 	
 	BEGIN {
-
-		$CertificateSourcePath = "C:\Temp\Cert"
-        $Certificates = Get-ChildItem -Path $CertificateSourcePath | Select *
-		
+		$ImportFilePath = "C:\Temp"
+		$ImportFileName = "HSPDPID_BB.csv"
+		$SamX509Cert = Import-Csv "$ImportFilePath\$ImportFileName"
+		$DomainAdmin = "Fargo\Administrator"
+		$Password = Read-Host "Enter Admin Password" -AsSecureString
+		$DomainCredential = new-object -typename System.Management.Automation.PSCredential -argumentlist $DomainAdmin, $Password
 	} #end BEGIN
 	
 	PROCESS {
-		
-		foreach ($PIVCert in $Certificates) {
+		foreach ($Identity in $SamX509Cert) {
 			
-            $X509Cert = New-Object system.security.cryptography.X509Certificates.X509Certificate $($PIVCert.FullName)
-			$X509CertIssuerPath = [Regex]::Replace($X509Cert.Issuer, ',\s*(CN=|OU=|O=|DC=|C=)', '!$1') -split "!"
-			$X509CertSubjectPath = [Regex]::Replace($X509Cert.subject, ',\s*(CN=|OU=|O=|DC=|C=)', '!$1') -split "!"
-			$ADAdminUserName = "{0}-adm" -f $($PIVCert.BaseName)
-			
-            $X509CertIssuer = ""
-			# Reverse the path and save as $issuer
-			for ($i = $X509CertIssuerPath.count - 1; $i -ge 0; $i--) {
-				$X509CertIssuer += $X509CertIssuerPath[$i]
-				if ($i -ne 0) {
-					$X509CertIssuer += ","
-				}
-			} #end for $X509CertIssuer
-						
-			$X509CertSubject = ""
-			# Reverse the path and save as $issuer
-			for ($i = $X509CertSubjectPath.count - 1; $i -ge 0; $i--) {
-				$X509CertSubject += $X509CertSubjectPath[$i]
-				if ($i -ne 0) {
-					$X509CertSubject += ","
-				}
-			} #end for $X509CertSubject
-			
-			# Format as required for altSecurityIdentities
-			$NewX509Cert = "X509:<I>{0}<S>{1}" -f $X509CertIssuer,$X509CertSubject
+			$ADAdminUserName = "{0}-adm" -f $Identity.sAMAccountName <#Builds the first.last-adm from the first.last name #>
+			$X509Name = ($Identity.sAMAccountName).split(".").toupper() -join (" ") <# 8, Builds the Subject CN=FIRST LAST name #>
+			$NewX509CertIC = 'C=US' <# 0 #>
+            $NewX509CertIO = 'O=Entrust' <# 1 #>
+            $NewX509CertIOU01 = 'OU=Certification Authorities' <# 2 #>
+            $NewX509CertIOU02 = 'OU=Entrust Managed Services SSP CA' <# 3 #>
+            $NewX509CertSC = 'C=US' <# 4 #>
+            $NewX509CertSO = 'O=U.S. Government' <# 5 #>
+            $NewX509CertSOU ='OU=Department of Agriculture' <# 6 #>
+            $NewX509CertSCN01 = 'CN=' <# 7 #>
+            $NewX509CertSCN02 = ' OID.0.9.2342.19200300.100.1.1=1200' <# 9, Note: Leading Space before OID #>
 
-            Switch ($PRODUCTION) {
-				LIVE { <# Set the AD user's Cert #> set-aduser -identity $ADAdminUserName -Add @{ 'altsecurityidentities' = $NewX509Cert } }
-				REVIEW { <# Review $NewX509Cert #> "$ADAdminUserName `n $NewX509Cert" }
-				} #end Switch-Production
+            $NewX509Cert = "X509:<I>{0},{1},{2},{3}<S>{4},{5},{6},{7}{8}{9}{10}" -f 
+            $NewX509CertIC,
+            $NewX509CertIO,
+            $NewX509CertIOU01,
+            $NewX509CertIOU02,
+            $NewX509CertSC,
+            $NewX509CertSO,
+            $NewX509CertSOU,
+            $NewX509CertSCN01,
+            $X509Name,
+            $NewX509CertSCN02,
+            $($Identity.HSPDPID)
 
+			#$NewX509Cert = "X509:<I>C=US,O=Entrust,OU=Certification Authorities,OU=Entrust Managed Services SSP CA<S>C=US,O=U.S. Government,OU=Department of Agriculture,CN=FIRST LAST OID.0.9.2342.19200300.100.1.1=12001234567890"
 			
-		}
+			Switch ($PRODUCTION) {
+				LIVE { set-aduser -identity $ADAdminUserName -Add @{ 'altsecurityidentities' = $NewX509Cert } -Credential $DomainCredential }
+				REVIEW {
+					$ADAdminUserName
+					$NewX509Cert
+				}
+			} #end Switch PRODUCTION
+		} #end foreach
 	} #end PROCESS
 	
 	END {
 		
 	} #end END
-} #end Set-ADUserX509Cert
+} #end Set-ADUSER_X509_CERT
