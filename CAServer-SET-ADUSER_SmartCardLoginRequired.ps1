574c96e87b7a5b533e79a70dac904fbd124a8e3b
harringg <elists@ghmultimedia.com>
2015-08-04 16:51:55 -0500

    Added custom objects and output to CSV reporting

--- CAServer-SET-ADUSER_SmartCardLoginRequired.ps1
+++ CAServer-SET-ADUSER_SmartCardLoginRequired.ps1
@@ -20,7 +20,7 @@ function Set-SmartCardLoginRequired {
             AUTHOR: Grant Harrington
             EMAIL: grant.harrington@ars.usda.gov
             CREATED: 7/7/2015 6:39 AM
-			LASTEDIT: 8/3/2015 3:22 PM
+			LASTEDIT: 8/4/2015 4:50 PM
             KEYWORDS: ADUC, Lincpass
 	.LINK
 		EAD Scripts
@@ -40,22 +40,33 @@ function Set-SmartCardLoginRequired {
 		# This will place all first.last-adm accounts into an array
 		# All users will have the SmartcardLogonRequired attribute set to True (when run Live)
 		#$SetLoginRequiredADM = Get-ADUser -Filter { sAMAccountName -like "*-adm" }
-		$SetLoginRequiredADM = Get-ADUser -Filter { UserPrincipalName -like "1200*" -and Department -like "*RU*"}
+		$SetLoginRequiredADM = Get-ADUser -Filter { UserPrincipalName -like "1200*" -and Department -like "*RU*"} -Properties * | select *
 		#This will store the credentials as Secure Text on the client PC running the script
 		$DomainAdmin = "Fargo\Administrator"
 		$Password = Read-Host "Enter Admin Password" -AsSecureString
 		$DomainCredential = new-object -typename System.Management.Automation.PSCredential -argumentlist $DomainAdmin, $Password
-		
+		$date = get-date -f yyMMddHHmm
 	} #end BEGIN
 	
 	PROCESS {
-		
+		$ListUsers = @() 
 		foreach ($LoginRequired in $SetLoginRequiredADM) {
 			
 			Switch ($PRODUCTION) {
 				LIVE { Set-ADUser -Identity $($LoginRequired.sAMAccountName) -SmartcardLogonRequired:$true -Credential $DomainCredential }
 				REVIEW {
-					$($LoginRequired.sAMAccountName)
+					$LoginRequiredProps = [ordered]@{
+						sAMAccountName = $LoginRequired.sAMAccountName
+                        UserPrincipalName = "1200xxxxxxxxxx@fedidcard.gov"
+						Department = $LoginRequired.Department
+						LastLogonDate = $LoginRequired.LastLogonDate
+						SmartcardLogonRequired = $LoginRequired.SmartcardLogonRequired
+                        ReportRun = $date
+						
+					} #end Win32_tpmProps
+
+					$LoginRequiredObj = New-Object -TypeName PSObject -Property $LoginRequiredProps
+					$ListUsers += $LoginRequiredObj
 				}
 			} #end Switch PRODUCTION
 		} #end foreach
@@ -63,6 +74,7 @@ function Set-SmartCardLoginRequired {
 	} #end PROCESS
 	
 	END {
-		
+		$ListUsers | sort sAMAccountName | ft
+        $ListUsers | Export-csv "C:\temp\CER-PreMFAEnforce-$date.csv" -NoTypeInformation
 	} #end END
 } #end Set-SmartCardLoginRequired
