6b5dccbc15768d9070fcc29bdf00897ad2105b9e
harringg <elists@ghmultimedia.com>
2016-07-11 17:23:30 -0500

    Update ws2_32dll.ps1
    Added code to get current version (matches GUI values)

--- NESSUS/ws2_32dll.ps1
+++ NESSUS/ws2_32dll.ps1
@@ -3,3 +3,10 @@
 
 get-item "C:\Windows\System32\ws2_32.dll" |
 select @{n='ComputerName';e={$env:COMPUTERNAME}},FullName,CreationTime,@{n='Version';e={$_.versioninfo.productversion}} | fl
+
+# [Modified, 7/11/2016 5:22 PM, Grant Harrington]
+# Modified from reference: https://blogs.technet.microsoft.com/askpfeplat/2014/12/07/how-to-correctly-check-file-versions-with-powershell/
+$DLLVersionInfomation = (get-item $env:windir\System32\ws2_32.dll).VersionInfo | select *
+$DLLVersionInfomation.FileName
+$DLLVersionInfomation.ProductBuildPart
+$DLLVersionInfomation.ProductPrivatePart
