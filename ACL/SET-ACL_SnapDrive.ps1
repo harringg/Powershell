ebb65073421b5d6cfc4a48044fa2a4c391862c36
harringg <elists@ghmultimedia.com>
2016-07-14 14:25:52 -0500

    Created dynamic variables for file names and paths

--- ACL/SET-ACL_SnapDrive.ps1
+++ ACL/SET-ACL_SnapDrive.ps1
@@ -1,14 +1,25 @@
 #[Modified, 7/12/2016 7:16 PM, Grant Harrington]
+#[Modified, 7/14/2016 2:24 PM, Grant Harrington]
 
-$SNAPDriveModify = Import-CSV "C:\SCRIPTS\Units-Snap-ANS-IGB.CSV"
-#RUFolder,IdentityReference
+$Date = get-date -f yyMMdd_HHmm
+$ImportCSVPath = "C:\SCRIPTS"
+$ImportCSVFile = "Units-Snap-ANS-IGB"
+$ImportCSV = "{0}\{1}.csv" -f $ImportCSVPath,$ImportCSVFile
+$SNAPDriveModify = Import-Csv $ImportCSV
+$OutFileFile = "{0}\Logs\{1}_{2}_Log.txt" -f $ImportCSVPath,$Date,$ImportCSVFile
+
+<#
+RUFolder,IdentityReference
+"E:\Units\Animals\Snap","USDA\ARSLNDFAR-ANS-DFS-MODIFY"
+#>
+
 foreach ($RU in $SNAPDriveModify) {
 $RUFolder = "$($RU.RUFolder)"
 $startDate = get-date
 $StartScreen = Write-Host "Started processing folder $RUFolder $startdate"
 $StartScreen
 $StartLog = "Started processing folder $RUFolder $startdate"
-$StartLog | Out-File "C:\Scripts\Logs\ACL-SNAP-ANS-IGB.txt" -Append
+$StartLog | Out-File $OutFileFile -Append
 icacls $RUFolder
 
 $FileSystemRights = 'Modify, Synchronize'
@@ -28,6 +39,6 @@ $endScreen = Write-Host "Completed Processing $RUFolder $enddate"
 $endScreen
 $endLog = "Completed Processing $RUFolder $enddate"
 $icacls = icacls $RUFolder
-$icacls | Out-File "C:\Scripts\Logs\ACL-SNAP-ANS-IGB.txt" -Append
-$endLog | Out-File "C:\Scripts\Logs\ACL-SNAP-ANS-IGB.txt" -Append
+$icacls | Out-File $OutFileFile -Append
+$endLog | Out-File $OutFileFile -Append
 }
