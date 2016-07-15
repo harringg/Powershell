768c6352116f208c6f3092fac3f493230eaa0a5d
harringg <elists@ghmultimedia.com>
2016-07-14 20:50:50 -0500

    Replaced Write-Host with -Verbose
    Updated Start/Stop text as -Verbose for onscreen, was Write-Host

--- ACL/SET-ACL_LabFolderAccessRestriction.ps1
+++ ACL/SET-ACL_LabFolderAccessRestriction.ps1
@@ -12,7 +12,7 @@
 $Domain = 'USDA'
 $RU = 'CER'
 $RUPrevious = 'Cereals'
-$Directory = 'LAB-FARIS' #'PII'
+$Directory = 'LAB-FRIESEN' #'PII'
 $BaseFolder = "E:\Units\{0}\Snap\{1}" -f $RUPrevious,$Directory
 #$RUFolder = "E:\Units\COMMON\{0}\{1}" -f $RU,$Directory
 $ADUCSG_Remove = "ARSLNDFAR-{0}-DFS-MODIFY" -f $RU
@@ -23,6 +23,7 @@ $Date = get-date -f yyMMdd_HHmm
 $OutFilePath= "C:\SCRIPTS"
 $OutFileFile = "{0}\Logs\{1}_{2}_SetFolderRestrictions_Log.txt" -f $OutFilePath,$Date,$Directory
 
+<#
 #region Test Variables
 $BaseFolder
 $ADUCSG_Remove
@@ -38,14 +39,14 @@ $TestList
 $TestModify = Get-ADGroup $ADUCSG_Modify
 $TestModify
 #endregion
+#>
 
 $startDate = get-date
-$StartScreen = Write-Host "Started processing folder $BaseFolder $startdate"
-$StartScreen
 $StartIcacls = icacls $BaseFolder
-$StartLog = "Started processing folder $BaseFolder $startdate"
+$StartLogMessage = "Started processing folder $BaseFolder $startdate"
+Write-verbose $StartLog -Verbose
 $StartIcacls
-$StartLog | Out-File $OutFileFile -Append
+$StartLogMessage | Out-File $OutFileFile -Append
 $StartIcacls | Out-File $OutFileFile -Append
 
 # STEP 01
@@ -119,10 +120,10 @@ $ACL.AddAccessRule($ACE) #TODO set vs add, which should be used
 Set-Acl -Path $BaseFolder -AclObject $ACL
 #endregion
 
-$endDate = get-date
-$endScreen = Write-Host "Completed Processing $BaseFolder $enddate"
-$endScreen
-$endLog = "Completed Processing $BaseFolder $enddate"
+$EndDate = get-date
 $EndIcacls = icacls $BaseFolder
+$EndLogMessage = "Completed Processing $BaseFolder $enddate"
+Write-verbose $EndLogMessage -Verbose
+$EndIcacls
 $EndIcacls | Out-File $OutFileFile -Append
-$endLog | Out-File $OutFileFile -Append
+$EndLogMessage | Out-File $OutFileFile -Append
