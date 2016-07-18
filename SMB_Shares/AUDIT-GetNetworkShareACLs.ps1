2e9511b3677806a1a6ced5e164197c8d3e03e216
harringg <elists@ghmultimedia.com>
2016-07-18 14:35:53 -0500

    Added Log output
    Modified write-host to write-verbose, added Log output

--- SMB_Shares/AUDIT-GetNetworkShareACLs.ps1
+++ SMB_Shares/AUDIT-GetNetworkShareACLs.ps1
@@ -1,31 +1,61 @@
-﻿#[Modified, 7/18/2016 8:50 AM, Grant Harrington]
+﻿# [Modified, 7/18/2016 8:50 AM, Grant Harrington]
+# [Modified, 7/18/2016 2:08 PM, Grant Harrington]
 cls
-Write-Host
-Write-Host "Audit-GetNetworkShareACLs.ps1"
- 
+
+$DateRun = get-date
+$Date = Get-date -f yyMMdd_HHmm
+
+$ScriptName = 'Audit-GetNetworkShareACLs.ps1'
+$ScriptPath = 'https://github.com/harringg/Powershell/blob/master/SMB_Shares/AUDIT-GetNetworkShareACLs.ps1'
+Write-verbose "Script Name: $ScriptName" -Verbose
 #$path = Read-host “Enter a UNC Path: ”
-$Path = "\\10.x.x.x\Public\IT"
+$PathBase = '10.170.180.2'
+$PathShare = 'COMMON'
+$PathShareNested = 'OCD'
+
+$OutputFilePath= "C:\SCRIPTS"
+$OutFileFile = "{0}\Logs\{1}_{2}_Share_ACL_Log.txt" -f $OutputFilePath,$Date,$PathShare
+
+$ScriptName | Out-File $OutFileFile -Append
+$ScriptPath | Out-File $OutFileFile -Append
+
+if ($PathShareNested) {
+$path = "\\{0}\{1}\{2}" -f $PathBase,$PathShare,$PathShareNested
+} else{
+$path = "\\{0}\{1}" -f $PathBase,$PathShare
+}
+
+$startLog = "Started Processing $Path $DateRun"
+$startLog | Out-File $OutFileFile -Append
+
+#$Path = "\\10.170.180.2\COMMON"
 $pathparts = $path.split("\")
 $ComputerName = $pathparts[2]
 $ShareName = $pathparts[3]
 $SharePath = "{0}\{1}" -f $ComputerName,$ShareName
  
-Write-Host "File Sharing Permissions Report - $path"
-Write-Host
-get-date
-Write-Host 
-Write-Host "File/NTFS Permissions"
-Write-Host
- 
+Write-verbose "File Sharing Permissions Report - $path" -Verbose
+"File Sharing Permissions Report - $path" | Out-File $OutFileFile -Append
+Write-verbose "File/NTFS Permissions" -Verbose
+"File/NTFS Permissions" | Out-File $OutFileFile -Append
 Write-Host "Network Path: $Path"
-Get-acl $Path | fl
-icacls $Path
-((Get-Item $Path).GetAccessControl('Access').Access) #This gets the root directory ACL
+"Network Path: $Path" | Out-File $OutFileFile -Append
+$GetACL = Get-acl $Path | fl
+Write-Output $GetACL
+$icacls = icacls $Path
+Write-Output $icacls
+$GetItemACL = ((Get-Item $Path).GetAccessControl('Access').Access) #This gets the root directory ACL
+Write-Output $GetItemACL
+Write-Verbose "Share/SMB Permissions" -Verbose
+Write-Verbose "   Share Path: $SharePath" -Verbose
 
-Write-Host
-Write-Host "Share/SMB Permissions"
-Write-Host
-Write-Host "   Share Path: $SharePath"
+'Get-ACL $Path' | Out-File $OutFileFile -Append
+$GetACL | Out-File $OutFileFile -Append
+'icacls $Path' | Out-File $OutFileFile -Append
+$icacls | Out-File $OutFileFile -Append
+'((Get-Item $Path).GetAccessControl(''Access'').Access)' | Out-File $OutFileFile -Append
+$GetItemACL | Out-File $OutFileFile -Append
+
     $Share = Get-WmiObject win32_LogicalShareSecuritySetting -Filter "name='$ShareName'" -ComputerName $ComputerName
     if($Share){
         $obj = @()
@@ -40,7 +70,12 @@ Write-Host "   Share Path: $SharePath"
                 1245631 {$Perm = "Change"}
                 1179817 {$Perm = "Read"}
             }
-            Write-Host "   $Domain\$user  $Perm"
+     #       Write-Host "   $Domain\$user  $Perm"
+            Write-verbose "   $Domain\$user  $Perm" -Verbose
+            "   $Domain\$user  $Perm" | Out-File $OutFileFile -Append
         }
     }
-Write-Host
+
+$enddate = get-date
+$endLog = "Completed Processing $Path $enddate"
+$endLog | Out-File $OutFileFile -Append
