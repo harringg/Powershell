710871dcb1a75bd41ec79b06c0de148f956e00a7
harringg <elists@ghmultimedia.com>
2016-07-19 16:10:13 -0500

    Added IF statement and Select ID

--- EAD/GPMC/Get-RSGPOAll.ps1
+++ EAD/GPMC/Get-RSGPOAll.ps1
@@ -42,8 +42,13 @@ function Get-RSGPO {
 	
 	PROCESS {
     
+    if ($RSGPOAll) {
+    Write-Verbose "`$RSGPOAll Variable has been created"
+} else {
     Write-Verbose "Gathering all GPOs in USDA.net domain and assinging to `$RSGPOAll Variable"
-	$global:RSGPOAll = get-gpo -all | select DisplayName
+	$global:RSGPOAll = get-gpo -all | select DisplayName,Id}
+
+
 	} #end PROCESS
 	
 	END {
