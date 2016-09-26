d45a13d87528fa02eed523074433e39698aa6756
harringg <elists@ghmultimedia.com>
2016-09-25 19:28:30 -0500

    Initial commit

---
+++ EAD/OS/USMT/SourcePC-ScanState.ps1
@@ -0,0 +1,136 @@
+﻿$ScriptName = SourcePC-ScanState.ps1
+
+#region ScanState USMT
+$PCName = $env:COMPUTERNAME
+$LoggedInUser = $env:USERNAME
+
+$StorPath = 'C:\User State Migration Tool\{0}_{1}\' -f $PCName,$LoggedInUser
+$MigApp = '/i:migapp.xml'
+$MigDocs = '/i:migdocs.xml' #Read in this documents configuration file
+$MigUser = '/i:miguser.xml' #Read in this user configuration file
+$UserExclude = '/ue:*\*' #Exclude Domain\User
+$PrimaryUser = 'first.last'
+$UserInclude = "/ui:usda\{0}" -f $PrimaryUser #Include Domain\User
+$ContinueOnNonFatalErrors = '/c' #Continue on non-fatal errors
+$Overwrite = '/o' #Overwrite
+$LocalOnly = '/localonly' #exclude the data from removable drives and network drives mapped on the source computer
+$All = '/all' #Migrates all of the users on the computer.
+
+& scanstate $StorPath $MigApp $MigDocs $MigUser $UserExclude $UserInclude $LocalOnly $ContinueOnNonFatalErrors $Overwrite
+C:\User State Migration Tool\x86\> 
+
+#C:\User State Migration Tool\x86\> scanstate "C:\User State Migration Tool\store\" /i:migapp.xml /i:migdocs.xml /i:miguser.xml /ue:*\* /ui:usda\lucas.brandt /c /o
+#endregion
+
+#region Get Profile ID using Registry ProfileList
+$FargoSIDPattern = 'S-1-5-21-2670568672'
+$USDASIDPattern = 'S-1-5-21-2443529608'
+
+Push-Location #Records the current network path
+$ProfileListPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList'
+Set-Location $ProfileListPath
+$ProfileToRemove = Get-ChildItem -Path $ProfileListPath | % { Get-ItemProperty $_.pspath} | select ProfileImagePath,PSChildName | Where {$_.PSChildName -like "$FargoSIDPattern*"}
+
+Get-ChildItem -Path $ProfileListPath | % { Get-ItemProperty $_.pspath} | select ProfileImagePath,PSChildName | Where {$_.PSChildName -like "$FargoSIDPattern*"}
+Pop-Location #Restores the network path recorded in Push-Location
+
+Push-Location #Records the current network path
+$ProfileGuidPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileGuid'
+Set-Location $ProfileGuidPath
+Get-ChildItem -Path $ProfileGuidPath | % { Get-ItemProperty $_.pspath} | select SidString,PSChildname | Where {$_.SidString -like "$FargoSIDPattern*"}
+Pop-Location #Restores the network path recorded in Push-Location
+#endregion
+
+#https://social.technet.microsoft.com/Forums/windowsserver/en-US/73c6ca2b-2f7f-4a35-9895-07eac0de1456/powershell-delete-profiles-from-registry?forum=winserverpowershell
+
+Get-ChildItem ’HKLM:\Software\Microsoft\Windows NT\CurrentVersion\ProfileList’ |
+    ForEach-Object{
+        $profilepath=$_.GetValue('ProfileImagePath')    
+        if($profilepath -notmatch 'administrator|Ctx_StreamingSvc|NetworkService|Localservice|systemprofile|grant.harrington'){
+            Write-Host "Removing item: $profilepath" -ForegroundColor green
+            #Remove-Item $_.PSPath -Whatif
+        }else{
+            Write-Host "Skipping item:$profilepath" -Fore blue -Back white
+	    }
+    }
+
+$First = 'Bill'
+$Last = 'Kemp'
+$LastSixChar = ("$Last"[0..6] -join "").ToLower()     # "Harrington" returns the string 'harring'
+$FirstOneChar = ("$First"[0..0] -join "").ToLower()   # "Grant" returns the string 'g'
+$ModernProfile = "{0}.{1}" -f $First,$Last
+$HistoricProfile = "{0}{1}" -f $LastSixChar,$FirstOneChar
+
+$ModernProfile
+$HistoricProfile
+
+$objectpath = "C:\Users\"
+
+Remove-Item -path "$objectpath*" -Exclude Administrator, Default, Public -Confirm
+
+Get-Childitem -path "$objectpath*" | Out-Gridview -Title "REMAINING USER ACCOUNTS ON YOUR COMPUTER"
+Get-Childitem -path "$objectpath*" | select FullName
+
+##################### 160923 ################################
+
+
+$FargoProfilePath = "C:\Users\rs.grant.harrington-"
+$ProfileDirectory = $FargoProfilePath -replace [regex]"^C:\\Users\\",''
+$FileName = '/f'
+$Recurse = '/r'
+$GiveOwnershipToAdmins = '/a'
+$Prompt = '/d'
+$TakeOwnership = 'y' # Y = Take Ownership, N = Skip
+Push-Location #Records the current network path
+$ProfileRoot = 'C:\Users'
+Set-Location $ProfileRoot #Set Session Path to ProfileRoot
+# Take Ownership of $ProfileDirectory (required to allow removal of Fargo SID Profile Directory)
+# takeown /f "$ProfileDirectory" /r /a /d y
+takeown $FileName "$ProfileDirectory" $Recurse $GiveOwnershipToAdmins $Prompt $TakeOwnership
+#If errors occur, rerun TakeOwn
+
+Remove-Item -path "$FargoProfilePath" -Force
+#If errors (Access Denied) occur, rerun takeown
+
+icacls "CSC-Recovery" /grant "Everyone:(F)" /t /l /q
+
+Remove-Item -path "$RemovePath" -Force
+
+Get-Childitem -path "$objectpath*" | Select * | ft
+
+###
+# What this is doing
+# Getting
+$DetermineDomainProfiles = Get-CimInstance Win32_UserProfile | Select SID, LastUseTime, LocalPath, Loaded | sort SID
+($DetermineDomainProfiles | ForEach-Object {
+	if (($_.SID -match [regex]"^$FargoSIDPattern.*") -eq $TRUE) {
+		
+        (($_) | ForEach-Object {
+			$global:FargoDomainAccountsAll_Array = @()
+			
+			$FargoDomainAccountsProperties = [ORDERED]@{
+				'LocalPath' = ($_.LocalPath)
+				'SID' = ($_.SID)
+				
+			} #end $FargoDomainAccountsProperties
+			
+			$FargoDomainAccountsAll = New-Object -TypeName PSCustomObject -Property $FargoDomainAccountsProperties
+			$global:FargoDomainAccountsAll_Array += $FargoDomainAccountsAll
+			$FargoDomainAccountsAll_Array
+		}) -split ','
+	} #end if CN=.*\\
+	elseif (($_.SID -match [regex]"^$USDASIDPattern.*") -eq $TRUE) {
+		($_) | ForEach-Object { $_.LocalPath, $_.SID }
+	} #end elseif (CN=|,OU.*$)
+} #end foreach-object
+) -Split ','###
+
+# What do I want to do?
+# Get Name,LastWrite of C:\Users Dirctory
+# Remove "Fargo" Profiles
+# -Determine by SID and create Fargo or USDA String
+# Remove "Extra" (Fargo/USDA) Profiles Folders, helpdesk, non-active users, etc..
+# Identify Profiles to be removed in Registry (based on folders removed above)
+# Remove Registry values
+# -ProfileList
+# -ProfieGUID
---
+++ EAD/OS/USMT/USMT-Workflow.ps1
@@ -0,0 +1,306 @@
+﻿#region List Profile Directories that exist in C:\Users (In Explorer)
+cls
+# Set Profile Path Variable
+$ProfilePath = 'C:\Users'
+
+# Change Path to ProfilePath
+Set-Location $ProfilePath
+
+# Get All Profiles Installed on this PC
+$UserProfileDirectory = Get-Childitem -path "$ProfilePath\*" | select FullName, BaseName
+
+# Report All Profiles Installed on this PC
+$UserProfileDirectory
+#endregion
+
+#region List Registry ProfileList values that exist
+
+# Records the current network path
+Push-Location
+
+# Set Registry ProfileList Path Variable
+$ProfileListPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList'
+
+# Set Path to $ProfileListPath value
+Set-Location $ProfileListPath
+
+# Gather ProfileListPathAll value, Lists all values in ProfileList Registry entry
+#$ProfileListPathAll = Get-ChildItem -Path $ProfileListPath | % { Get-ItemProperty $_.pspath } | select ProfileImagePath, PSChildName
+$ProfileListPathAll = Get-ChildItem -Path $ProfileListPath | % { Get-ItemProperty $_.pspath }
+
+#$ProfileListPathAll | sort ProfileImagePath
+#$ProfileListPathAll | sort PSChildName
+
+$global:ProfileLISTUSDA_Array = @()
+$global:ProfileLISTFARGO_Array = @()
+$global:ProfileLISTAllOthers_Array = @()
+
+$FargoSIDPattern = 'S-1-5-21-2670568672'
+$USDASIDPattern = 'S-1-5-21-2443529608'
+
+foreach ($ProfileLIST in $ProfileLISTPathAll) {
+	
+	if ($ProfileLIST -match "$USDASIDPattern*") {
+		#region $global:ProfileLISTUSDA_Array
+		ForEach ($LIST in $ProfileLIST) {
+			
+			$ProfileLISTUSDAProperties = [ORDERED]@{
+				'USDAProfileImagePath' = ($LIST.ProfileImagePath) -join ','
+				'USDAGuid' = ($LIST.Guid) -join ','
+				'USDAPSPath' = ($LIST.PSPath) -join ','
+				'USDAPSParentPath' = ($LIST.PSParentPath) -join ','
+				'USDAPSChildName' = ($LIST.PSChildName) -join ','
+			} #end $ProfileLISTUSDAProperties
+			
+			$ProfileLISTUSDA = New-Object -TypeName PSCustomObject -Property $ProfileLISTUSDAProperties
+			$global:ProfileLISTUSDA_Array += $ProfileLISTUSDA
+		} #end ForEach ($LIST in $ProfileLIST) $ProfileLISTUSDA_Array
+		#endregion  $global:ProfileLISTUSDA_Array	
+	} #end if
+	elseif ($ProfileLIST -match "$FargoSIDPattern*") {
+		#region $global:ProfileLISTFARGO_Array	
+		
+		ForEach ($LIST in $ProfileLIST) {
+			
+			$ProfileLISTFARGOProperties = [ORDERED]@{
+				'FARGOProfileImagePath' = ($LIST.ProfileImagePath) -join ','
+				'FARGOGuid' = ($LIST.Guid) -join ','
+				'FARGOPSPath' = ($LIST.PSPath) -join ','
+				'FARGOPSParentPath' = ($LIST.PSParentPath) -join ','
+				'FARGOPSChildName' = ($LIST.PSChildName) -join ','
+			} #end $ProfileLISTUSDAProperties
+			
+			$ProfileLISTFARGO = New-Object -TypeName PSCustomObject -Property $ProfileLISTFARGOProperties
+			$global:ProfileLISTFARGO_Array += $ProfileLISTFARGO
+			
+		} #end ForEach ($LIST in $ProfileLIST) $ProfileLISTFARGO_Array
+		#endregion  $global:ProfileLISTFARGO_Array
+	} #end ForEach ($LIST in $ProfileLIST) ProfileLISTFARGO
+	
+	elseif ($ProfileLIST -notmatch "$FargoSIDPattern*|$USDASIDPattern*") {
+		#region $global:ProfileLISTAllOthers_Array
+		#"This is neither Fargo nor USDA LIST: $ProfileLIST"
+		#$ProfileLIST | select *
+		ForEach ($LIST in $ProfileLIST) {
+			
+			$ProfileLISTAllOthersProperties = [ORDERED]@{
+				'OTHERProfileImagePath' = ($LIST.ProfileImagePath) -join ','
+				'OTHERGuid' = ($LIST.Guid) -join ','
+				'OTHERPSPath' = ($LIST.PSPath) -join ','
+				'OTHERPSParentPath' = ($LIST.PSParentPath) -join ','
+				'OTHERPSChildName' = ($LIST.PSChildName) -join ','
+			} #end $ProfileLISTAllOthersProperties
+			
+			$ProfileLISTAllOthers = New-Object -TypeName PSCustomObject -Property $ProfileLISTAllOthersProperties
+			$global:ProfileLISTAllOthers_Array += $ProfileLISTAllOthers
+		} #end $global:ProfileLISTAllOthers_Array foreach
+		#endregion  $global:ProfileLISTAllOthers_Array
+		
+	} #end elseif ($ProfileLIST -notmatch "$FargoSIDPattern*|$USDASIDPattern*")
+}
+
+$ProfileLISTUSDA_Array | fl
+$ProfileLISTAllFARGO_Array | fl
+$ProfileLISTAllOthers_Array | fl
+#Restores the network path recorded in Push-Location
+Pop-Location
+
+# This will remove all FARGO Domain GUID values that were collected
+foreach ($PSPath in $ProfileLISTAllFARGO_Array.FargoPSPath) {
+	#Remove-Item $PSPath -WhatIf
+}
+
+#endregion List Registry ProfileList values that exist
+
+
+#region List Registry ProfileGUID values that exist
+
+# Records the current network path
+Push-Location
+
+# Set Registry ProfileGuid Path Variable
+$ProfileGuidPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileGuid'
+
+# Set Path to $ProfileGuidPath value
+Set-Location $ProfileGuidPath
+
+# Gather ProfileGUIDPathAll value, Lists all values in ProfileGUID Registry entry
+$ProfileGUIDPathAll = Get-ChildItem -Path $ProfileGuidPath | % { Get-ItemProperty $_.pspath }
+#$ProfileGUIDPathAll = Get-ChildItem -Path $ProfileGuidPath | % { Get-ItemProperty $_.pspath } | select SidString, PSChildname
+#$ProfileGUIDPathAll | Sort SidString
+
+$global:ProfileGUIDUSDA_Array = @()
+$global:ProfileGUIDFARGO_Array = @()
+$global:ProfileGUIDAllOthers_Array = @()
+
+$FargoSIDPattern = 'S-1-5-21-2670568672'
+$USDASIDPattern = 'S-1-5-21-2443529608'
+
+foreach ($ProfileGUID in $ProfileGUIDPathAll) {
+	
+	if ($ProfileGUID -match "$USDASIDPattern*") {
+		#$ProfileGUID | select *
+		
+		#region $global:ProfileGUIDUSDA_Array
+		ForEach ($GUID in $ProfileGUID) {
+			
+			$ProfileGUIDUSDAProperties = [ORDERED]@{
+				'USDASIDString' = ($GUID.SidString) -join ','
+				'USDAPSPath' = ($GUID.PSPath) -join ','
+				'USDAPSParentPath' = ($GUID.PSParentPath) -join ','
+				'USDAPSChildName' = ($GUID.PSChildName) -join ','
+			} #end $ProfileGUIDUSDAProperties
+			
+			$ProfileGUIDUSDA = New-Object -TypeName PSCustomObject -Property $ProfileGUIDUSDAProperties
+			$global:ProfileGUIDUSDA_Array += $ProfileGUIDUSDA
+		} #end $global:ProfileGUIDUSDA_Array foreach
+		#endregion  $global:ProfileGUIDUSDA_Array
+		
+	}
+	elseif ($ProfileGUID -match "$FargoSIDPattern*") {
+		#$ProfileGUID | select *
+		
+		#region $global:ProfileGUIDFARGO_Array
+		ForEach ($GUID in $ProfileGUID) {
+			
+			$ProfileGUIDFARGOProperties = [ORDERED]@{
+				'FARGOSIDString' = ($GUID.SidString) -join ','
+				'FARGOPSPath' = ($GUID.PSPath) -join ','
+				'FARGOPSParentPath' = ($GUID.PSParentPath) -join ','
+				'FARGOPSChildName' = ($GUID.PSChildName) -join ','
+			} #end $ProfileGUIDUSDAProperties
+			
+			$ProfileGUIDFARGO = New-Object -TypeName PSCustomObject -Property $ProfileGUIDFARGOProperties
+			$global:ProfileGUIDFARGO_Array += $ProfileGUIDFARGO
+		} #end $global:ProfileGUIDFARGO_Array foreach
+		#endregion  $global:ProfileGUIDFARGO_Array
+	}
+	elseif ($ProfileGUID -notmatch "$FargoSIDPattern*|$USDASIDPattern*") {
+		#"This is neither Fargo nor USDA GUID: $ProfileGUID"
+		#$ProfileGUID | select *
+		#region $global:ProfileGUIDAllOthers_Array
+		ForEach ($GUID in $ProfileGUID) {
+			
+			$ProfileGUIDAllOthersProperties = [ORDERED]@{
+				'OTHERSIDString' = ($GUID.SidString) -join ','
+				'OTHERPSPath' = ($GUID.PSPath) -join ','
+				'OTHERPSParentPath' = ($GUID.PSParentPath) -join ','
+				'OTHERPSChildName' = ($GUID.PSChildName) -join ','
+			} #end $ProfileGUIDAllOthersProperties
+			
+			$ProfileGUIDAllOthers = New-Object -TypeName PSCustomObject -Property $ProfileGUIDAllOthersProperties
+			$global:ProfileGUIDAllOthers_Array += $ProfileGUIDAllOthers
+		} #end $global:ProfileGUIDAllOthers_Array foreach
+		#endregion  $global:ProfileGUIDAllOthers_Array
+		
+	}
+}
+$ProfileGUIDUSDA_Array | fl
+$ProfileGUIDFARGO_Array | fl
+$ProfileGUIDAllOthers_Array
+Pop-Location #Restores the network path recorded in Push-Location
+
+# This will remove all FARGO Domain GUID values that were collected
+foreach ($PSPath in $ProfileGUIDFARGO_Array.FargoPSPath) {
+	#Remove-Item $PSPath -WhatIf
+}
+
+#endregion
+
+
+# Enter profile to migrate (static variable)
+$UserToUSMT = 'grant.harrington'
+# OR (Allows you to enter text dymnamically)
+# $UserToUSMT = Read-Host "Enter Directory To Migrate using USMT"
+
+# Build ProfileImagePath variable
+$ProfileImagePath = "{0}\{1}" -f $ProfilePath, $UserToUSMT
+
+
+$ProfileToRemove = Get-ChildItem -Path $ProfileListPath | % { Get-ItemProperty $_.pspath } | select ProfileImagePath, PSChildName | Where { $_.ProfileImagePath -eq "$ProfileImagePath" }
+
+#Restores the network path recorded in Push-Location
+Pop-Location
+
+#endregion
+
+
+
+
+# If PSChildName (aka SID) matches the FARGO SID, we will remove the Registry Value
+$FargoSIDPattern = 'S-1-5-21-2670568672'
+$USDASIDPattern = 'S-1-5-21-2443529608'
+if ($ProfileToRemove.PSChildName -like "$FargoSIDPattern*") {
+	"This Profile is FARGO SID and will be removed"
+	Remove-Item $_.PSPath -Whatif
+}
+else {
+	"This Profile is a non-FARGO SID and will be ignored"
+}
+
+
+#region ProfileGUID_Array
+$global:ProfileGUID_Array = @()
+
+ForEach ($GUID in $ProfileGUIDPathAll) {
+	
+	
+	$ProfileGUIDProperties = [ORDERED]@{
+		'MySIDString' = ($GUID.SidString)
+		'MyPSPath' = ($GUID.PSPath)
+		'MyPSParentPath' = ($GUID.PSParentPath)
+		'MyPSChildName' = ($GUID.PSChildName)
+	} #end AuditADGroupProperties
+	
+	$ProfileGUID = New-Object -TypeName PSCustomObject -Property $ProfileGUIDProperties
+	$global:ProfileGUID_Array += $ProfileGUID
+	
+	
+}
+$ProfileGUID_Array | fl
+#endregion
+
+
+#region ProfileList_Array
+$global:ProfileList_Array = @()
+
+ForEach ($LIST in $ProfileListPathAll) {
+	
+	
+	$ProfileListProperties = [ORDERED]@{
+		'MyProfileImagePath' = ($LIST.ProfileImagePath)
+		'MyGUID' = ($LIST.GUID)
+		'MyPSPath' = ($LIST.PSPath)
+		'MyPSParentPath' = ($LIST.PSParentPath)
+		'MyPSChildName' = ($LIST.PSChildName)
+	} #end AuditADGroupProperties
+	
+	$ProfileList = New-Object -TypeName PSCustomObject -Property $ProfileListProperties
+	$global:ProfileList_Array += $ProfileList
+	
+	
+}
+$ProfileList_Array | ft -AutoSize
+#endregion
+
+
+$ProfileLISTUSDA_Array.USDAPSChildName | sort
+$ProfileLISTFARGO_Array.FARGOPSChildName | sort
+$ProfileLISTAllOthers_Array.OTHERPSChildName | sort
+
+
+$ProfileGUIDUSDA_Array.USDASIDString | sort
+$ProfileGUIDFARGO_Array.FARGOSIDString | sort
+$ProfileGUIDAllOthers_Array.OTHERSIDString | sort
+
+ForEach-Object
+if ($ProfileLISTUSDA_Array.USDAPSChildName -match $ProfileGUIDUSDA_Array.USDASIDString) {
+	"Do Something"
+}
+else {
+	"Opps"
+}
+
+$MyPSPath = 'Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileGuid\{7ffa8fa7-433f-4807-ae79-be1285b5a648}'
+$MyPSPath2 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileGuid\{7ffa8fa7-433f-4807-ae79-be1285b5a648}'
+Remove-Item $MyPSPath2 -Whatif
