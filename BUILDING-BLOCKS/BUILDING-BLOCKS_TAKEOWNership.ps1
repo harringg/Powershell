b01bfcc99cd288ce398c596ae37a75ffe726c631
harringg <elists@ghmultimedia.com>
2016-09-26 14:19:48 -0500

    Initial commit

---
+++ BUILDING-BLOCKS/BUILDING-BLOCKS_TAKEOWNership.ps1
@@ -0,0 +1,16 @@
+﻿takeown /?
+
+$ParentDirectory = 'C:\Users'
+$ChildDirectory = 'first.last'
+$FilenameOrDirectory = '/f'
+$Recurse = '/r'
+$GiveOwnershipToAdministrators = '/a'
+$DefaultAnswer = '/d'
+$TakeOwnership = 'y'
+$Skip = 'n'
+Push-Location
+cd $ParentDirectory
+
+takeown $FilenameOrDirectory $ChildDirectory $Recurse $GiveOwnershipToAdministrators $DefaultAnswer $TakeOwnership
+
+takeown /f $ChildDirectory /r /a /d y
