cafc13772b884aa677cbb01a0f675bc681b86cbf
harringg <elists@ghmultimedia.com>
2016-09-27 20:12:26 -0500

    Initial commit

---
+++ EAD/OS/WIN10/Add-AppxPackage.ps1
@@ -0,0 +1,3 @@
+﻿#[Modified, 8/5/2016 2:48 PM, Grant Harrington]
+# Reinstall built-in applications
+Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
