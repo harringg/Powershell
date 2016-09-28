68ca0fa5ca3ef6453adeae062f1fbf0b47e3cd1b
harringg <elists@ghmultimedia.com>
2016-09-27 20:16:53 -0500

    Initial commit

---
+++ EAD/ADUC/AUDIT/Audit-PCLastBootTime.ps1
@@ -0,0 +1,5 @@
+﻿#http://www.powershelladmin.com/wiki/Find_last_boot_up_time_of_remote_Windows_computers_using_WMI
+$PCName = 'ARSNDFAR5WHRDZ1'
+$LastBootUpTime = Get-WmiObject Win32_OperatingSystem -Comp $PCName | Select -Exp LastBootUpTime
+Write-host "Last Boot Time for: $PCName"
+[System.Management.ManagementDateTimeConverter]::ToDateTime($LastBootUpTime)
