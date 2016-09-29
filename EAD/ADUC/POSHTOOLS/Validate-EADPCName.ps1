e0a58c1e93dca36442a672c638e0914066dd7a78
harringg <elists@ghmultimedia.com>
2016-09-29 17:04:51 -0500

    Initial commit

---
+++ EAD/ADUC/POSHTOOLS/Validate-EADPCName.ps1
@@ -0,0 +1,113 @@
+﻿function Validate-EADPCName {
+<#
+	.SYNOPSIS
+		
+	Validate that PC
+	.DESCRIPTION
+		
+	
+	.PARAMETER PRODUCTION
+		This is used to review your results before actually running the script
+		REVIEW run the script to review results before running live in production enviroment
+		Verb-Noun -PRODUCTION REVIEW
+		LIVE Will run the script live in production enviroment
+		Verb-Noun -PRODUCTION LIVE
+	
+	.PARAMETER REPORTTYPE
+		
+	
+	.EXAMPLE
+		PS C:\> Export-GPOReports -ReportType HTML -PRODUCTION REVIEW
+		PS C:\> Export-GPOReports -ReportType HTML -PRODUCTION LIVE
+	
+	.OUTPUTS
+		System.Array
+	
+	.NOTES
+		NAME: Export-GPOReports
+		AUTHOR: Grant Harrington
+		EMAIL: grant.harrington@ars.usda.gov
+		CREATED: 2/11/2016 7:48 PM
+		LASTEDIT: 2/11/2016 7:48 PM
+		KEYWORDS:
+	
+	.LINK
+		EAD Scripts
+#>
+	[CmdletBinding(SupportsPaging = $true,
+				   SupportsShouldProcess = $true)]
+	[OutputType([array])]
+	param
+	(
+		[Parameter(Mandatory = $TRUE)]
+		[ValidateSet('REVIEW', 'LIVE')]
+		[string]$PRODUCTION = 'REVIEW',
+		[Parameter(Mandatory = $TRUE)]
+		[ValidateSet('FAR', 'EGF')]
+		[string]$DeviceLocation = 'FAR',
+		[Parameter(Mandatory = $TRUE)]
+		[ValidateSet('DESKTOP', 'LAPTOP')]
+		[string]$DeviceType,
+		[Parameter(Mandatory = $TRUE)]
+		[string]$SerialNumber
+	)
+	
+	BEGIN {
+		$Date = Get-Date -format yyMMdd
+		
+		switch ($DeviceLocation) {
+			FAR {
+				$Location = 'ARSNDFAR'
+			} #end FAR
+			EGF {
+					$Location = 'ARSMNEGF'
+			} #end EGF
+		} #end Switch
+		
+		switch ($DeviceType) {
+			DESKTOP {
+				$Device = '4'
+			} #end DESKTOP
+			LAPTOP {
+				$Device = '5'
+			} #end LAPTOP
+		} #end Switch
+		
+	} #end BEGIN
+	
+	PROCESS {
+		
+		
+		Switch ($PRODUCTION) {
+			LIVE {
+                # Builds PC Name based on Location (FAR/EGF), Device Type (DESKTOP/LAPTOP), and last six of Device Serial Number
+				$global:EADName = "{0}{1}{2}" -f $Location, $Device, $SerialNumber
+                # Checks that name equals 15 charachters ((ARSNDFAR = 8) + (4 = 1) + (123456 = 6) = 15)
+				if ($EADName.length -eq '15') {
+					Write-Host "This PC, $EADName, is properly named to meet EAD Naming Standards" -ForegroundColor White -BackgroundColor Green
+                    # If PC name is correct, check is ran to see if it's located in EAD
+                    $EADCheck = Get-ADComputer -Identity $EADname -ErrorAction Ignore
+                        # IF PC name is correct and is located in EAD, "Do Something"
+                        if ($EADCheck -like "*DC=USDA*") {
+                            $EADCheck
+                            $EADCheck = $ExistingPC
+                            $ExistingPC
+                        } else {
+                            Write-Host "$EADName does not exist in Enterprise Active Directory"
+                        } #end if ($EADCheck -like "*DC=USDA*")
+                    
+				} else {
+                    Write-Host "This PC does not meet EAD Naming Standards, it is $($EADName.length) charachters and requires 15" -ForegroundColor White -BackgroundColor DarkRed
+                    BREAK
+                } #end if ($EADName.length -eq '15')
+			} # end LIVE
+			REVIEW {
+			} #end REVIEW
+		} #end Switch-Production
+		
+	} #end PROCESS
+	
+	END {
+		
+	} #end END
+} #end Validate-EADPCName
