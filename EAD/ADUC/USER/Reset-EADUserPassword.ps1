d887b6bc52315ac7a9354d8ab95a7d589f3e01da
harringg <elists@ghmultimedia.com>
2016-10-18 11:22:49 -0500

    Initial commit

---
+++ EAD/ADUC/USER/Reset-EADUserPassword.ps1
@@ -0,0 +1,67 @@
+﻿Import-Module ActiveDirectory
+function Reset-EADUserPassword {
+	<#
+	.SYNOPSIS
+	
+	.DESCRIPTION
+	
+	.PARAMETER REPORTTYPE
+
+	.PARAMETER PRODUCTION
+		This is used to review your results before actually running the script
+        REVIEW run the script to review results before running live in production enviroment
+        Verb-Noun -PRODUCTION REVIEW
+        LIVE Will run the script live in production enviroment
+        Verb-Noun -PRODUCTION LIVE
+	
+	.EXAMPLE
+				PS C:\> Export-GPOReports -ReportType HTML -PRODUCTION REVIEW
+	            PS C:\> Export-GPOReports -ReportType HTML -PRODUCTION LIVE
+	.NOTES
+		    NAME: Reset-EADUserPassword
+            AUTHOR: Grant Harrington
+            EMAIL: grant.harrington@ars.usda.gov
+            CREATED: 10/18/2016 11:12 AM
+			LASTEDIT: 10/18/2016 11:12 AM
+            KEYWORDS:
+	.LINK
+		EAD Scripts
+#>
+	[CmdletBinding(SupportsPaging = $true,
+				   SupportsShouldProcess = $true)]
+	[OutputType([array])]
+	param
+	(
+		[Parameter(Mandatory = $TRUE)]
+		[ValidateSet('REVIEW', 'LIVE')]
+		[string]$PRODUCTION = 'REVIEW',
+		[Parameter(Mandatory = $TRUE)]
+		[string]$SearchUser
+	)
+	
+	BEGIN {
+		$Date = Get-Date -format yyMMdd
+        $ImportedPW = Import-Csv 'C:\Users\grant.harrington\Documents\GitHub\Powershell\EAD\ADUC\USER\DefaultPassword.csv'
+		$DefaultPassword = ConvertTo-SecureString -string $ImportedPW.DefaultPassword -AsPlainText -force
+		$UserAccount = Get-ADUser $SearchUser -Properties Description,Enabled,PasswordLastSet
+	} #end BEGIN
+	
+	PROCESS {
+		
+		
+		Switch ($PRODUCTION) {
+			LIVE {
+				Set-ADAccountPassword $UserAccount -NewPassword $DefaultPassword –Reset
+			} # end LIVE
+			REVIEW {
+                $UserAccount
+                $ImportedPW.DefaultPassword
+			} #end REVIEW
+		} #end Switch-Production
+		
+	} #end PROCESS
+	
+	END {
+		
+	} #end END
+} #end Reset-EADUserPassword
