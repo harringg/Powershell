f98a0716a121b56c8a888e649a0be2c9f1e80484
harringg <elists@ghmultimedia.com>
2016-10-18 15:50:35 -0500

    Converted to accept multiple PCs and select output

--- ACL/Get-LocalAdmin.ps1
+++ ACL/Get-LocalAdmin.ps1
@@ -1,5 +1,4 @@
-﻿function Get-LocalAdmin
-{
+﻿function Get-LocalAdmin {
 	<#
 	.SYNOPSIS
 		Default Synopsis
@@ -18,7 +17,7 @@
             AUTHOR: Grant Harrington
             EMAIL: grant.harrington@ars.usda.gov
             CREATED: 8/10/2016 8:25 PM
-			LASTEDIT: 8/10/2016 8:25 PM
+			LASTEDIT: 10/18/2016 3:49 PM
             KEYWORDS:
 	.LINK
 		EAD Scripts
@@ -28,36 +27,60 @@
 	[OutputType([array])]
 	param
 	(
-		[Parameter(Mandatory = $False)]
-		[string]$computerName
+		[Parameter(Mandatory = $True)]
+		[ValidateSet('SinglePC', 'ManualList', 'ImportList')]
+		[string]$PCListInput,
+		[Parameter(Mandatory = $True)]
+		[ValidateSet('Screen', 'ExportCSV')]
+		[string]$OutputType
 	)
 	
-	BEGIN
-	{
-        $PCList = 'localhost', 'ARSNDFAR4806072'
-		#$group = Get-WmiObject win32_group -ComputerName $computerName -Filter "LocalAccount=True AND SID='S-1-5-32-544'"
-	} #end BEGIN
-
-	PROCESS
-	{
-        $PCList |
-        ForEach-Object {
-        	$group = Get-WmiObject win32_group -ComputerName $_ -Filter "LocalAccount=True AND SID='S-1-5-32-544'"
-        $query = "GroupComponent = `"Win32_Group.Domain='{0}'`,Name='{1}'`"" -f $group.domain, $group.name
-
-        $list = Get-wmiobject win32_groupuser -ComputerName $_ -Filter $query
-        $list |
-        ForEach-Object {
-            $_.PartComponent} |
-            ForEach-Object {
-            $_.substring($_.lastindexof("Domain=") + 7).replace("`",Name=`"","\") 
-            }
-        }
+	BEGIN {
+	
+    	switch ($PCListInput) {
+			'SinglePC' { $PCList = Read-Host "Enter PC Name" }
+			'ManualList' { }
+			'ImportList' { }
+		} #end Switch PCListInput
+		
+		$PCList = 'localhost' #, 'ARSNDFAR4806072'
+		$LocalGroupFilter = "LocalAccount=True AND SID='S-1-5-32-544'"
+	
+    } #end BEGIN
+	
+	PROCESS {
+		$PCList |
+		ForEach-Object {
+			$LocalGroup = Get-WmiObject win32_group -ComputerName $_ -Filter $LocalGroupFilter
+			$WMIQuery = "GroupComponent = `"Win32_Group.Domain='{0}'`,Name='{1}'`"" -f $LocalGroup.domain, $LocalGroup.name
+			
+			$AdminList = Get-wmiobject win32_groupuser -ComputerName $_ -Filter $WMIQuery
+			
+		} # end ForEach-Object $PCList pipe
+		
+		$global:AdminGroupMembersAll_Array = @()
+		$AdminGroupMembersProperties = @{
+			'PCName' = $PCList
+			'Objects' = ($AdminList |
+			ForEach-Object {
+				$_.PartComponent
+			} <# end PartComponent#> |
+			ForEach-Object {
+				$_.substring($_.lastindexof("Domain=") + 7).replace("`",Name=`"", "\")
+			} <# end Substring#>) -join ','
+		} # end AdminGroupMembersProperties
+		
+		$AdminGroupMembersAll = New-Object -TypeName PSCustomObject -Property $AdminGroupMembersProperties
+		$global:AdminGroupMembersAll_Array += $AdminGroupMembersAll
+		
 	} #end PROCESS
 	
-	END
-	{
-		
+	END {
+		switch ($OutputType) {
+			'Screen' { $AdminGroupMembersAll_Array | fl }
+			'ManualList' { }
+			'ImportList' { }
+		} #end Switch OutputType
 	} #end END
 } #end Get-LocalAdmin
 
