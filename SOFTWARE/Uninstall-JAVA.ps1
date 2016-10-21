010352a36b9706d0a9f1b472ac14cfad20dd4b38
harringg <elists@ghmultimedia.com>
2016-10-21 14:52:51 -0500

    Initial commit

---
+++ SOFTWARE/Uninstall-JAVA.ps1
@@ -0,0 +1,37 @@
+﻿ #################################### Assumed Actions ###########################################
+## This script is running on a computer that is known to have the sofware we're looking for on it
+################################################################################################
+
+
+## Use the SCCM WMI class SMS_InstalledSoftware to see if the software is registered there
+$InstalledProducts = Get-WmiObject -Namespace 'root\cimv2\sms' -Class SMS_InstalledSoftware -ComputerName localhost
+## To review the installed products (optional)
+#$InstalledProducts | Sort ProductName | select ProductName
+
+## Partial name of software title you are looking for
+$SoftwareSearch = "activ"
+$Java = $InstalledProducts | where { $_.ARPDisplayName -imatch $SoftwareSearch } | Select ARPDisplayName,LocalPackage,ProductVersion,Publisher,UninstallString,SoftwareCode
+$Java
+
+# Product name = ARPDisplayName
+# Location of cached MSI = LocalPackage
+# Product Version = ProductVersion
+# Publisher = Publisher
+# Uninstall String = UninstallString
+# GUID = SoftwareCode
+
+#To confirm your selecting the correct software package, run this command
+$Java.ARPDisplayName[0]
+$Java.ProductVersion[0]
+
+$RemoveJava= $InstalledProducts | where { ($_.ARPDisplayName -eq "$($Java.ARPDISPLAYNAME[0])") -and ($_.ProductVersion -eq "$($Java.ProductVersion[0])") }
+ 
+## Create the code to put in our software upgrade script
+## To make the uninstall silent an extremely common set of switches for msiexec is /qn (quiet with no interface)
+$CachedMSI = $RemoveJava.LocalPackage
+
+## The cached MSI's path is always in the LocalPackage property under each instance in SMS_InstalledSoftware
+## /x = Uninstall
+## /qn = (q) all defaults (n) no UI appears
+
+#Start-Process 'msiexec.exe' -ArgumentList "/x $CachedMSI /qn" -Wait -NoNewWindow
