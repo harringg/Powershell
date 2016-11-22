5e4a020fedcc8757e0760b1a8c121903c2d20ba5
harringg <elists@ghmultimedia.com>
2016-11-22 16:04:14 -0600

    Initial commit

---
+++ SOFTWARE/Install-JunosPulse.ps1
@@ -0,0 +1,11 @@
+﻿Push-Location #Records the current network path
+$PulseInstallerPath = '\\usda.net\ars\PA3060\INSTALLERS\JUNIPER_VPN'
+$PulseFolderName = 'Juniper_JunosPulse_5.1.61601'
+$PulseExe = 'JunosPulse.x64.msi'
+$JNPRPreconfigFile = 'LincPass.jnprpreconfig'
+$InstallerRoot = "{0}\{1}" -f $PulseInstallerPath,$PulseFolderName
+
+Set-Location $InstallerRoot
+
+& msiexec /i $PulseExe CONFIGFILE=$JNPRPreconfigFile ALLUSERS=1 /qn /norestart /log output.log
+Pop-Location #Restores the network path recorded in Push-Location
